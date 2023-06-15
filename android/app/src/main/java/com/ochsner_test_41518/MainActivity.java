a8f31a197f12aa0a4a66d2eceaee6bfc198ca330
Crowdbotics <team@crowdbotics.com>
2023-06-15 20:09:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ochsner_test_41518/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ochsner_test_41518;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ochsner_test_41518";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ochsner-test-41518",
+  url: "https://ochsner-test-41518.botics.co",
+  api: "https://ochsner-test-41518.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
